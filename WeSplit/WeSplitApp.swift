7d5cbacc3cdb4613cdf3b5d80b7e8515e3a93653
Mansi Shah <mansi.iosdev@gmail.com>
2020-12-26 22:47:43 +0530

    WeSplit app

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mansi Shah on 23/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
